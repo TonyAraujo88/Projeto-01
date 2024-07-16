26fbef8b9abae31673f23b37d64993c065d13605
TonyAraujo88 <tonybugattieb@gmail.com>
2024-07-16 15:57:09 -0300

    DevClub

---
+++ SitetestDevClub/script.js
@@ -0,0 +1,9 @@
+function calcularDolar() {
+    let real = document.getElementById("input-real").value
+    let dolar = document.getElementById("input-dolar").value
+
+    let resultado = real / dolar
+
+    alert(resultado) 
+} 
+
